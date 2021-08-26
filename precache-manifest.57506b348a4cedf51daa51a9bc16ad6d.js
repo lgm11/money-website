806803be5a5f10b02e5f843bb59638cf2e5dd1a7
lee <1079811936@qq.com>
2021-08-26 13:27:27 +0800

    修改页面主色调

---
+++ precache-manifest.57506b348a4cedf51daa51a9bc16ad6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3431385c04f02a186f0",
+    "url": "/money-website/css/app.75e9c00c.css"
+  },
+  {
+    "revision": "f414d2b58ea5eb0055bc051e543aeb04",
+    "url": "/money-website/index.html"
+  },
+  {
+    "revision": "b3431385c04f02a186f0",
+    "url": "/money-website/js/app.ae6f2148.js"
+  },
+  {
+    "revision": "e7b1cbb1f60ad137e6d9",
+    "url": "/money-website/js/chunk-vendors.e6f65227.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/money-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-website/robots.txt"
+  }
+]);
